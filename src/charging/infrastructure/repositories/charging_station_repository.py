6cf4aa3988a07a84f985dfd1716503570458be55
Urwa23 <urwahmaqsood23@gmail.com>
2025-01-14 17:49:26 +0100

    charging station repository code added

--- src/charging/infrastructure/repositories/charging_station_repository.py
+++ src/charging/infrastructure/repositories/charging_station_repository.py
@@ -1,9 +1,19 @@
 from typing import List
-
 from charging.domain.value_objects.postal_code import PostalCode
 from charging.domain.aggregates.charging_station import ChargingStation
 
 
 class ChargingStationRepository:
+    def __init__(self):
+        self._stations = [
+            ChargingStation(id="1", name="Station A", postal_code="12345"),
+            ChargingStation(id="2", name="Station B", postal_code="12345"),
+            ChargingStation(id="3", name="Station C", postal_code="67890"),
+        ]
+
     def find_by_postal_code(self, postal_code: PostalCode) -> List[ChargingStation]:
-        raise NotImplementedError
+        return [
+            station
+            for station in self._stations
+            if station.postal_code == str(postal_code)
+        ]
